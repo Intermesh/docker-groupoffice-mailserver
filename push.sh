b357965529a44062d6a78a9f4025b60fbda51517
Merijn Schering <mschering@intermesh.nl>
2024-12-09 16:22:32 +0100

    6.8 tag

--- push.sh
+++ push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 docker login
-docker buildx build --push --platform linux/amd64,linux/arm64 --no-cache . -t intermesh/groupoffice-mailserver:latest
+
+docker pull debian:bookworm-slim
+
+docker buildx build --push --platform linux/amd64,linux/arm64 --no-cache . -t intermesh/groupoffice-mailserver:latest -t intermesh/groupoffice-mailserver:6.8
 
 #docker push intermesh/groupoffice-mailserver:latest
